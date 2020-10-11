502c03c41d452d73b68a806546b055f21eab886b
lovit <soy.lovit@gmail.com>
2020-10-11 20:42:19 +0900

    Reordering functions by sequence (#2)

--- KoBERTScore/score.py
+++ KoBERTScore/score.py
@@ -2,147 +2,6 @@ import torch
 import torch.nn.functional as F
 
 
-def bert_forwarding(bert_model, input_ids=None, attention_mask=None, output_layer_index=-1):
-    """
-    Args:
-        bert_model (transformers`s Pretrained models)
-        input_ids (torch.LongTensor) : (batch, max seq len)
-        attention_mask (torch.LongTensor) : (batch, max seq len)
-        output_layer_index (int or str)
-            The index of last BERT layer which is used for token embedding
-            If type of `output_layer_index` is `str`, it returns hidden states of all layers
-
-    Returns:
-        hidden_states (torch.tensor) : (B, K, D) or (n_layers, B, K, D)
-            B : batch size
-            K : maximum sequence length in `input_ids`
-            D : BERT embedding dim
-    """
-    with torch.no_grad():
-        _, _, hidden_states = bert_model(
-            input_ids, attention_mask=attention_mask, output_hidden_states=True)
-    if output_layer_index == 'all':
-        return hidden_states
-    return hidden_states[output_layer_index]
-
-
-def sents_to_tensor(bert_tokenizer, input_sents):
-    """
-    Args:
-        bert_tokenizer (transformers.PreTrainedTokenizer)
-        input_sents (list of str)
-
-    Returns:
-        padded_input_ids (torch.LongTensor) : (batch, max seq len)
-        attention_mask (torch.LongTensor) : (batch, max seq len)
-        token_mask (torch.LongTensor) : (batch, max seq len)
-            True token is 1 and padded / cls / sep token is 0
-
-    Examples::
-        >>> from transformers import BertTokenizer
-        >>> model_name = "bert-base-uncased"
-        >>> tokenizer = BertTokenizer.from_pretrained(model_name)
-        >>> input_sents = ['Hellow words', 'I am lovit', 'oh hello', 'where am I', 'where we go']
-        >>> sents_to_tensor(tokenizer, input_sents)
-        $ (tensor([[ 101, 7592, 2860, 2616,  102,    0,    0],
-                   [ 101, 1045, 2572, 8840, 5737, 2102,  102],
-                   [ 101, 2821, 7592,  102,    0,    0,    0],
-                   [ 101, 2073, 2572, 1045,  102,    0,    0],
-                   [ 101, 2073, 2057, 2175,  102,    0,    0]]),
-           tensor([[1, 1, 1, 1, 1, 0, 0],
-                   [1, 1, 1, 1, 1, 1, 1],
-                   [1, 1, 1, 1, 0, 0, 0],
-                   [1, 1, 1, 1, 1, 0, 0],
-                   [1, 1, 1, 1, 1, 0, 0]]),
-           tensor([[0, 1, 1, 1, 0, 0, 0],
-                   [0, 1, 1, 1, 1, 1, 0],
-                   [0, 1, 1, 0, 0, 0, 0],
-                   [0, 1, 1, 1, 0, 0, 0],
-                   [0, 1, 1, 1, 0, 0, 0]]))
-    """
-    inputs = bert_tokenizer.batch_encode_plus(input_sents, padding=True)
-    padded_input_ids = torch.LongTensor(inputs['input_ids'])
-    attention_mask = torch.LongTensor(inputs['attention_mask'])
-
-    zero_mask = torch.zeros(attention_mask.size(), dtype=torch.long)
-    token_mask = torch.where(padded_input_ids == bert_tokenizer.cls_token_id, zero_mask, attention_mask)
-    token_mask = torch.where(padded_input_ids == bert_tokenizer.sep_token_id, zero_mask, token_mask)
-    return padded_input_ids, attention_mask, token_mask
-
-
-def compute_pairwise_cosine(input_embeds, refer_embeds):
-    """
-    Args:
-        input_embeds (torch.tensor) : (B, K_i, D)
-            B : batch size
-            K_i : maximum sequence length in `input_embed`
-            D : BERT embedding dim
-        refer_embeds (torch.tensor) : (B, K_r, D)
-            B : batch size
-            K_r : maximum sequence length in `refer_embeds`
-            D : BERT embedding dim
-
-    Returns:
-        pairwise_cosine (torch.tensor) : (B, K_i, K_r)
-
-    Examples::
-        >>> input1 = torch.randn(3, 4, 5)
-        >>> input2 = torch.randn(3, 7, 5)
-        >>> compute_pairwise_cosine(input1, input2).size()
-        $ torch.Size([3, 4, 7])
-    """
-    def normalize(embeds):
-        embeds.div_(torch.norm(embeds, dim=-1).unsqueeze(-1))
-        return embeds
-
-    input_embeds = normalize(input_embeds)
-    refer_embeds = normalize(refer_embeds)
-    pairwise_cosine = torch.bmm(input_embeds, refer_embeds.permute(0, 2, 1))
-    return pairwise_cosine
-
-
-def compute_RPF(refer_embeds, hypoh_embeds, refer_weight_mask,
-                hypoh_weight_mask, idf=None, rescale_base=0):
-    """
-    Args:
-        refer_embeds (torch.tensor) : (B, K_i, D)
-            B : batch size
-            K_i : maximum sequence length in `input_embed`
-            D : BERT embedding dim
-        hypoh_embeds (torch.tensor) : (B, K_r, D)
-            B : batch size
-            K_r : maximum sequence length in `refer_embeds`
-            D : BERT embedding dim
-        refer_weight_mask (torch.tensor) : (batch, max seq len)
-            token mask or IDF weight mask
-        hypoh_weight_mask (torch.tensor) : (batch, max seq len)
-            token mask or IDF weight mask
-        idf (torch.nn.Embedding or None) : IDF weights
-        rescale_base (float) : 0 <= rescale_base < 1
-            Adjust (R-BERTScore - base) / (1 - base)
-
-    Returns:
-        R (torch.tensor) : R-BERTScore
-        P (torch.tensor) : P-BERTScore
-        F (torch.tensor) : F-BERTScore
-
-    """
-    pairwise_cosine = compute_pairwise_cosine(refer_embeds, hypoh_embeds)
-    R_max, _ = pairwise_cosine.max(dim=2)
-    P_max, _ = pairwise_cosine.max(dim=1)
-
-    if idf is not None:
-        refer_weight_mask = apply_idf(refer_ids, idf)
-        hypoh_weight_mask = apply_idf(hypoh_ids, idf)
-
-    R_max = rescaling(R_max, rescale_base)
-    P_max = rescaling(P_max, rescale_base)
-
-    R = (R_max * refer_weight_mask).sum(axis=1) / refer_weight_mask.sum(axis=1)
-    P = (P_max * hypoh_weight_mask).sum(axis=1) / hypoh_weight_mask.sum(axis=1)
-    F = 2 * (R * P) / (R + P)
-    return R, P, F
-
 def bert_score(bert_tokenizer, bert_model, references, hypotheses,
                idf=None, output_layer_index=-1, rescale_base=0):
     """
@@ -191,6 +50,148 @@ def bert_score(bert_tokenizer, bert_model, references, hypotheses,
         refer_weight_mask, hypoh_weight_mask,
         idf, rescale_base)
     return R, P, F
+
+
+def sents_to_tensor(bert_tokenizer, input_sents):
+    """
+    Args:
+        bert_tokenizer (transformers.PreTrainedTokenizer)
+        input_sents (list of str)
+
+    Returns:
+        padded_input_ids (torch.LongTensor) : (batch, max seq len)
+        attention_mask (torch.LongTensor) : (batch, max seq len)
+        token_mask (torch.LongTensor) : (batch, max seq len)
+            True token is 1 and padded / cls / sep token is 0
+
+    Examples::
+        >>> from transformers import BertTokenizer
+        >>> model_name = "bert-base-uncased"
+        >>> tokenizer = BertTokenizer.from_pretrained(model_name)
+        >>> input_sents = ['Hellow words', 'I am lovit', 'oh hello', 'where am I', 'where we go']
+        >>> sents_to_tensor(tokenizer, input_sents)
+        $ (tensor([[ 101, 7592, 2860, 2616,  102,    0,    0],
+                   [ 101, 1045, 2572, 8840, 5737, 2102,  102],
+                   [ 101, 2821, 7592,  102,    0,    0,    0],
+                   [ 101, 2073, 2572, 1045,  102,    0,    0],
+                   [ 101, 2073, 2057, 2175,  102,    0,    0]]),
+           tensor([[1, 1, 1, 1, 1, 0, 0],
+                   [1, 1, 1, 1, 1, 1, 1],
+                   [1, 1, 1, 1, 0, 0, 0],
+                   [1, 1, 1, 1, 1, 0, 0],
+                   [1, 1, 1, 1, 1, 0, 0]]),
+           tensor([[0, 1, 1, 1, 0, 0, 0],
+                   [0, 1, 1, 1, 1, 1, 0],
+                   [0, 1, 1, 0, 0, 0, 0],
+                   [0, 1, 1, 1, 0, 0, 0],
+                   [0, 1, 1, 1, 0, 0, 0]]))
+    """
+    inputs = bert_tokenizer.batch_encode_plus(input_sents, padding=True)
+    padded_input_ids = torch.LongTensor(inputs['input_ids'])
+    attention_mask = torch.LongTensor(inputs['attention_mask'])
+
+    zero_mask = torch.zeros(attention_mask.size(), dtype=torch.long)
+    token_mask = torch.where(padded_input_ids == bert_tokenizer.cls_token_id, zero_mask, attention_mask)
+    token_mask = torch.where(padded_input_ids == bert_tokenizer.sep_token_id, zero_mask, token_mask)
+    return padded_input_ids, attention_mask, token_mask
+
+
+def bert_forwarding(bert_model, input_ids=None, attention_mask=None, output_layer_index=-1):
+    """
+    Args:
+        bert_model (transformers`s Pretrained models)
+        input_ids (torch.LongTensor) : (batch, max seq len)
+        attention_mask (torch.LongTensor) : (batch, max seq len)
+        output_layer_index (int or str)
+            The index of last BERT layer which is used for token embedding
+            If type of `output_layer_index` is `str`, it returns hidden states of all layers
+
+    Returns:
+        hidden_states (torch.tensor) : (B, K, D) or (n_layers, B, K, D)
+            B : batch size
+            K : maximum sequence length in `input_ids`
+            D : BERT embedding dim
+    """
+    with torch.no_grad():
+        _, _, hidden_states = bert_model(
+            input_ids, attention_mask=attention_mask, output_hidden_states=True)
+    if output_layer_index == 'all':
+        return hidden_states
+    return hidden_states[output_layer_index]
+
+
+def compute_RPF(refer_embeds, hypoh_embeds, refer_weight_mask,
+                hypoh_weight_mask, idf=None, rescale_base=0):
+    """
+    Args:
+        refer_embeds (torch.tensor) : (B, K_i, D)
+            B : batch size
+            K_i : maximum sequence length in `input_embed`
+            D : BERT embedding dim
+        hypoh_embeds (torch.tensor) : (B, K_r, D)
+            B : batch size
+            K_r : maximum sequence length in `refer_embeds`
+            D : BERT embedding dim
+        refer_weight_mask (torch.tensor) : (batch, max seq len)
+            token mask or IDF weight mask
+        hypoh_weight_mask (torch.tensor) : (batch, max seq len)
+            token mask or IDF weight mask
+        idf (torch.nn.Embedding or None) : IDF weights
+        rescale_base (float) : 0 <= rescale_base < 1
+            Adjust (R-BERTScore - base) / (1 - base)
+
+    Returns:
+        R (torch.tensor) : R-BERTScore
+        P (torch.tensor) : P-BERTScore
+        F (torch.tensor) : F-BERTScore
+
+    """
+    pairwise_cosine = compute_pairwise_cosine(refer_embeds, hypoh_embeds)
+    R_max, _ = pairwise_cosine.max(dim=2)
+    P_max, _ = pairwise_cosine.max(dim=1)
+
+    if idf is not None:
+        refer_weight_mask = apply_idf(refer_ids, idf)
+        hypoh_weight_mask = apply_idf(hypoh_ids, idf)
+
+    R_max = rescaling(R_max, rescale_base)
+    P_max = rescaling(P_max, rescale_base)
+
+    R = (R_max * refer_weight_mask).sum(axis=1) / refer_weight_mask.sum(axis=1)
+    P = (P_max * hypoh_weight_mask).sum(axis=1) / hypoh_weight_mask.sum(axis=1)
+    F = 2 * (R * P) / (R + P)
+    return R, P, F
+
+
+def compute_pairwise_cosine(input_embeds, refer_embeds):
+    """
+    Args:
+        input_embeds (torch.tensor) : (B, K_i, D)
+            B : batch size
+            K_i : maximum sequence length in `input_embed`
+            D : BERT embedding dim
+        refer_embeds (torch.tensor) : (B, K_r, D)
+            B : batch size
+            K_r : maximum sequence length in `refer_embeds`
+            D : BERT embedding dim
+
+    Returns:
+        pairwise_cosine (torch.tensor) : (B, K_i, K_r)
+
+    Examples::
+        >>> input1 = torch.randn(3, 4, 5)
+        >>> input2 = torch.randn(3, 7, 5)
+        >>> compute_pairwise_cosine(input1, input2).size()
+        $ torch.Size([3, 4, 7])
+    """
+    def normalize(embeds):
+        embeds.div_(torch.norm(embeds, dim=-1).unsqueeze(-1))
+        return embeds
+
+    input_embeds = normalize(input_embeds)
+    refer_embeds = normalize(refer_embeds)
+    pairwise_cosine = torch.bmm(input_embeds, refer_embeds.permute(0, 2, 1))
+    return pairwise_cosine
 
 
 def apply_idf(ids, idf_embed):
